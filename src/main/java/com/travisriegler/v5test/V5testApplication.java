e7755410d58c09dd6c6338dcfe5dfa7388cdc664
Travis Riegler <travisriegler@protonmail.com>
2023-11-02 12:06:37 -0400

    init commit to main branch

---
+++ src/main/java/com/travisriegler/v5test/V5testApplication.java
@@ -0,0 +1,13 @@
+package com.travisriegler.v5test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V5testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V5testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travisriegler/v5test/V5testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travisriegler.v5test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V5testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
